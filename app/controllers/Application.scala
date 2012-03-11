9a32b10d91377af592f5b56d9bebad130dd1c713
Gaetan Renaudeau <renaudeau.gaetan@gmail.com>
2012-03-11 19:19:05 +0100

    add logs + disqus

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -11,18 +11,23 @@ object Application extends Controller {
   var counter = 0
   val hubEnum = Enumerator.imperative[JsValue]()
   val hub = Concurrent.hub[JsValue](hubEnum)
+
+  var nbClients = 0
   
   def index = Action {
     Ok(views.html.index("Play Painter"))
   }
 
   def stream = WebSocket.async[JsValue] { request =>
-    // Connected
+    nbClients += 1
     counter += 1 
     val pid = counter
     val out = Enumerator(
       JsObject(Seq("type" -> JsString("youAre"), "pid" -> JsNumber(pid))).as[JsValue]
     ).andThen(hub.getPatchCord())
+
+    Logger.info("(pid:"+pid+") connected.")
+    Logger.info(nbClients+" user(s) currently connected.");
 
     hubEnum push JsObject(Seq("type" -> JsString("connect"), "pid" -> JsNumber(pid))).as[JsValue]
 
@@ -32,6 +37,8 @@ object Application extends Controller {
       }
     }) mapDone { _ =>
       hubEnum push JsObject(Seq("type" -> JsString("disconnect"), "pid" -> JsNumber(pid))).as[JsValue]
+      nbClients -= 1
+      Logger.info("(pid:"+pid+") disconnected.")
     }
 
     Promise.pure( (in, out) )
