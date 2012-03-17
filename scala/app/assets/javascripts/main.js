a8de199e4b1728d435c15dceebf60a0141c92f8b
Gaetan Renaudeau <renaudeau.gaetan@gmail.com>
2012-03-17 14:49:01 +0100

    Ensure to not loose first messages

--- scala/app/assets/javascripts/main.js
+++ scala/app/assets/javascripts/main.js
@@ -146,27 +146,27 @@
 
     if (m.type == "disconnect") {
       delete players[m.pid];
+      return;
     }
-    else {
-      // clear local canvas if synchronized
-      if (m.type=="trace" && sentEventsCount[m.type] <= receivedEventsCount[m.type])
+
+    // clear local canvas if synchronized
+    if (m.type=="trace" && sentEventsCount[m.type] <= receivedEventsCount[m.type])
         meCtx.clearRect(0,0,meCtx.canvas.width,meCtx.canvas.height);
 
-      if (m.type == "trace") {
-        ctx.strokeStyle = player.color;
-        ctx.lineWidth = player.size;
-        ctx.beginPath();
-        var points = m.points;
-        ctx.moveTo(points[0].x, points[0].y);
-        points.forEach(function (p) {
-          ctx.lineTo(p.x, p.y);
-        });
-        ctx.stroke();
-        m.x = points[points.length-1].x;
-        m.y = points[points.length-1].y;
-      }
-      players[m.pid] = $.extend(players[m.pid], m);
+    if (m.type == "trace") {
+      ctx.strokeStyle = player.color;
+      ctx.lineWidth = player.size;
+      ctx.beginPath();
+      var points = m.points;
+      ctx.moveTo(points[0].x, points[0].y);
+      points.forEach(function (p) {
+        ctx.lineTo(p.x, p.y);
+      });
+      ctx.stroke();
+      m.x = points[points.length-1].x;
+      m.y = points[points.length-1].y;
     }
+    players[m.pid] = $.extend(players[m.pid], m);
 
     dirty_positions = true;
   }
