7860a7f802352f2aab6a798eb414907456789a50
Gaetan Renaudeau <gre@zenexity.com>
2013-05-07 01:05:51 +0200

    Update scala version to play 2.1.1

--- scala/project/Build.scala
+++ scala/project/Build.scala
@@ -1,17 +1,17 @@
 import sbt._
 import Keys._
-import PlayProject._
+import play.Project._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "Play Painter"
+    val appName         = "PlayPainter"
     val appVersion      = "1.0.1"
 
     val appDependencies = Seq(
       // Add your project dependencies here,
     )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    val main = play.Project(appName, appVersion, appDependencies).settings(
       // Add your own project settings here      
     )
 
